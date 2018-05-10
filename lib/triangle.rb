333dfede6fa8ab23274158fbb51179a6f0617c57
Eric DeSimone <eric.desimone@cortland.edu>
2018-05-10 12:38:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,9 +4,9 @@ class Triangle
   attr_reader :side_1, :side_2, :side_3, :all_sides
 
   def initialize(side_1:, side_2:, side_3:)
-    @side_1 = side_1:
-    @side_2 = side_2:
-    @side_3 = side_3:
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
     @all_sides = [@side_1, @side_2, @side_3]
     binding.pry
   end
