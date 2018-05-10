ef2053da319d07479173e2fc7f2787bcca70d6d3
Eric DeSimone <eric.desimone@cortland.edu>
2018-05-10 12:37:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :side_1, :side_2, :side_3, :all_sides
+
+  def initialize(side_1:, side_2:, side_3:)
+    @side_1 = side_1:
+    @side_2 = side_2:
+    @side_3 = side_3:
+    @all_sides = [@side_1, @side_2, @side_3]
+    binding.pry
+  end
+
+  def kind
+  end
+  
+end
+
+class TriangleError < StandardError
+  
 end
