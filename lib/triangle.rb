2effd7a482839adb63f948a72cab96957543d68f
Eric DeSimone <eric.desimone@cortland.edu>
2018-05-10 12:57:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,14 +10,13 @@ class Triangle
     @all_sides = [@side_1, @side_2, @side_3]
   end
 
-  def kind
-    if self.all_sides.any? {|side| side <= 0 } || if self.all_sides.sort! all_sides[0] + all_sides[1] < all_sides[2]
+  #def kind
+  #  if self.all_sides.any? {|side| side <= 0 } || if self.all_sides.sort! all_sides[0] + all_sides[1] < all_sides[2]
       raise TriangleError
-    end
-  end
+  #  end
+  #end
 
 class TriangleError < StandardError
-  "wrong fucking triangle"
 end
 
 end
