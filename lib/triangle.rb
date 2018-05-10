c980d19d7ae590b28075f96a3572aa8a0c95e83d
Eric DeSimone <eric.desimone@cortland.edu>
2018-05-10 12:59:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
 
   def kind
    if self.all_sides.any? {|side| side <= 0 } || if self.all_sides.sort! all_sides[0] + all_sides[1] < all_sides[2]
-    #  raise TriangleError
+    raise TriangleError
   end
   end
 
